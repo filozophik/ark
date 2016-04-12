92f1db68c1e5b6f58d8e5a5f251f9112be1b0a3d
Benjamin Lofo Follo <benjamin.lofofollo@hotmail.com>
2016-04-12 14:58:25 -0400

    Include Images and product work

--- angular/app/browse/ProductController.js
+++ angular/app/browse/ProductController.js
@@ -5,9 +5,55 @@
      * Created by lofo on 11/04/16.
      */
     angular.module('app.controllers').controller('ProductController', ['$scope', function($scope) {
-        $scope.products = [{
-            name:'Nick Jonas'
-        }];
+        $scope.products = [
+            {
+                description:'Light Open Dress w/ Silk Bottom',
+                category: 'dress',
+                subcategory: 'open',
+                gender:'F',
+                colors:['green'],
+                price: 10.00,
+                pictures : [
+                    'lime_1.gif',
+                    'lime_2.gif'
+                ]
+            },
+            {
+                description:'Rose Floral Dress',
+                category: 'dress',
+                subcategory: 'cut',
+                gender:'F',
+                colors:['red'],
+                price: 18.00,
+                pictures : [
+                    'rose_1.gif',
+                    'rose_2.gif'
+                ]
+            },
+            {
+                description:'Sakura Japanese Style Dress',
+                category: 'dress',
+                subcategory: 'full',
+                gender:'F',
+                colors:['green'],
+                price: 22.00,
+                pictures : [
+                    'sakura_1.gif',
+                    'sakura_2.gif'
+                ]
+            },
+            {
+                description:'Tropics Style Dress w/ Shoestring Shoulders',
+                category: 'dress',
+                subcategory: 'full',
+                gender:'F',
+                colors:['green'],
+                price: 13.00,
+                pictures : [
+                    'tropics_1.gif',
+                    'tropics_2.gif'
+                ]
+            }
+        ];
     }]);
-
 })();
