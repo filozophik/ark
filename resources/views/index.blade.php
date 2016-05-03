e43607d8e0d8463013654b3e7c3f9ebd4c142ff9
Benjamin Lofo Follo <benjamin.lofofollo@hotmail.com>
2016-05-03 16:25:47 -0400

    Sticky Footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,10 +15,11 @@
 </head>
 <body>
 <div ui-view="header"></div>
-<div ui-view="main"></div>
-<div ui-view="footer"></div>
+<div ui-view="main" class="main"></div>
 
+<div ui-view="footer"></div>
 <script src="/js/vendor.js"></script>
 <script src="/js/app.js"></script>
 </body>
+
 </html>
