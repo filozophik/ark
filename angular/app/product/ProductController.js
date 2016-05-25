65877e6ed6d0176beacc7270eb48363c28c6831d
Benjamin Lofo Follo <benjamin.lofofollo@hotmail.com>
2016-05-25 12:34:33 -0400

    Adjust display for new API endpoints

--- angular/app/product/ProductController.js
+++ angular/app/product/ProductController.js
@@ -9,14 +9,15 @@
      */
     angular.module('app.controllers').controller('ProductController', ['$scope', 'products', '$stateParams', function ($scope, products, $stateParams) {
         //Done Through REST
-        products.all().getList({limit:5}).then(function(resolve) {
-            $scope.related = resolve
-        },function(reject) {
-            console.log(reject);
-        });
+
         products.single($stateParams.id).get().then(function(resolve) {
             $scope.item = resolve;
         }, function(reject) {
+            console.log(reject);
+        });
+        products.single($stateParams.id).getList('related').then(function(resolve) {
+            $scope.related = resolve;
+        },function(reject) {
             console.log(reject);
         });
     }]);
